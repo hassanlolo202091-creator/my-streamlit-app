6750b5f24fa446a88f263de0e7a568f7a476573e
hassanlolo202091-creator <hassanlolo202091@gmail.com>
2026-06-26 22:58:45 +0400

    Update rovan_game.py

--- rovan_game.py
+++ rovan_game.py
@@ -100,6 +100,8 @@ elif st.session_state.step == 9:
     show_img("uni corne 3.jpeg")
     st.write("تم الإنقاذ! روفان بطلة الأبطال.")
     show_img("reward2.jpeg")
+     st.write("تم الإنقاذ! روفان بطلة الأبطال.")
+    show_img("reward1.jpeg")
     if st.button("إعادة اللعب"):
         st.session_state.step = 0
         st.rerun()
